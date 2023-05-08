4246c6132e643ba4a1f72c623bec34937b45e6f0
thermaltake <mertokaypolatogullari@gmail.com>
2023-05-08 19:02:50 +0300

    updatee

---
+++ src/main/java/Tasks.java
@@ -0,0 +1,59 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class Tasks {
+
+    /**
+     * Task 2
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     */
+
+    @Test
+    public void Task2()
+    {
+        given()
+
+                .when()
+                .get("https://httpstat.us/203")
+
+                .then()
+                .log().all()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+                ;
+
+    }
+
+    @Test
+    public void task1()
+    {
+        /** Task 1
+         * create a request to https://jsonplaceholder.typicode.com/todos/2
+         * expect status 200
+         * Converting Into POJO
+         */
+
+        ToDo todo=
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract().body().as(ToDo.class)
+
+
+                ;
+
+        System.out.println("todo = " + todo);
+        System.out.println("todo.getTitle() = " + todo.getTitle());
+    }
+}
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,86 @@
+public class ToDo {
+
+    int userId;
+    int id;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    String title;
+    Boolean completed;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{
+//"userId": 1,
+// "id": 2,
+//"title": "quis ut nam facilis et officia qui",
+//"completed": false
+//}
