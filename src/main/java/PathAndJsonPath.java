f4db54758fab7556f2c6fb4d51ebd03c2fb7d381
thermaltake <mertokaypolatogullari@gmail.com>
2023-05-12 04:04:26 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/PathAndJsonPath.java
+++ src/main/java/PathAndJsonPath.java
@@ -1,4 +1,6 @@
 import GoRest.User;
+import Model.Location;
+import Model.Place;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
@@ -67,6 +69,56 @@ public class PathAndJsonPath {
 
         System.out.println("usersPath = " + Arrays.toString(usersPath));
         System.out.println("usersJsonPath = " + usersJsonPath);
+    }
+
+    @Test
+    public void getUsersV1(){
+
+        Response body=
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                //log().body()
+                        .extract().response();
+
+        List<User> dataUsers=body.jsonPath().getList("data" , User.class);
+        //JSONPATH bir response içindeki bir parçayı nesneye dönüştürebiliriz.
+        System.out.println("dataUsers = " + dataUsers);
+
+        //Daha önceki örneklerde (as) Class dönüşümleri için tüm yapıya karşılık gelen
+        //gereken tüm classları yazarak dönüştürüp istediğimiz elemanlara ulaşıyorduk.
+        //Burada ise(JSONPATH) aradaki bir veriyi classa dönüştürerek bir list olarak atamamıza
+        //imkan veren JSONPATH i kullandık.Böylece tyek class ise veri alınmış oldu
+        //diğer class lara gerek kalmadan
+
+        //path: class  veya tip dönüşümüne imkan veremeyen direk veriyi verir.List<String> gibi
+        //JSONPATH : class dönüşümüne ve tip dönüşümüne izin vererek veriyi istediğimiz formatta verir.
+    }
+
+    @Test
+    public void getZipCode(){
+
+        Response response =
+
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .extract().response()
+
+                ;
+
+        Location locPathAs = response.as(Location.class); // Bütün classları yazmak zorundasın
+
+        System.out.println("locPathAs = " + locPathAs.getPlaces());
+
+        List<Place> places=response.jsonPath().getList("places", Place.class); // nokta atışı istediğimiz nesneyi aldık
+        System.out.println("places = " + places);
     }
 
 }
